c13294dae13a1364cd24d7995f6cfee067698c96
Jeongrak Choi <jrchoi0123@gmail.com>
2022-10-14 13:33:37 +0900

    slick slide

---
+++ src/components/ui/SlickSlide.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Slider from 'react-slick';
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import { useEffect, useState } from 'react';
+
+const SlickSlide = () => {
+    var settings = {
+        arrows: true,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+    return (
+        <div className="uiBox">
+            
+            <div className="sliderWrap">
+                <Slider {...settings}>
+                    <div>
+                        <img src="http://placeimg.com/830/480/any"/>
+                    </div>
+                    <div>
+                        <img src="http://placeimg.com/830/480/any"/>
+                    </div>
+                    <div>
+                        <img src="http://placeimg.com/830/480/any"/>
+                    </div>
+                    <div>
+                        <img src="http://placeimg.com/830/480/any"/>
+                    </div>
+                    <div>
+                        <img src="http://placeimg.com/830/480/any"/>
+                    </div>
+                    <div>
+                        <img src="http://placeimg.com/830/480/any"/>
+                    </div>
+                </Slider>
+            </div>
+
+        </div>
+    )
+}
+
+export default SlickSlide;
